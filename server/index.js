d0a17aaa0d3347a9c6189774500826fa2390eef5
Dave Clayton <davedx@gmail.com>
2014-09-20 21:52:42 +0200

    CORS

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.static("../client/"));
 
 app.all('*', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
 	next();
 });
 
