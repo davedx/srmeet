09ce3039751dce3cd1097c651b9b203ca8f66cd9
Dave Clayton <davedx@gmail.com>
2014-09-20 16:06:02 +0200

    Chat endpoints

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 var profile = require("./profile");
 var match = require("./match");
+var chat = require("./chat");
 
 app.use(bodyParser.json());
 
@@ -13,6 +14,7 @@ app.get('/', function (req, res) {
 
 profile(app);
 match(app);
+chat(app);
 
 MongoClient.connect('mongodb://127.0.0.1:27017/sameet', function(err, db) {
 	if(err) throw err;
