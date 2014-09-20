9cb69f41a74cee330e6b0b103978b227e6f0470b
Orlin <asenov@gmail.com>
2014-09-20 21:03:16 +0200

    facabook SDK is used

--- client/javascript/lib/main.js
+++ client/javascript/lib/main.js
@@ -1,62 +1,134 @@
 var mainApp = (function(){
-
+  
 	var isLoggedIn = function(){
-
+		
 		var session = hello( "facebook" ).getAuthResponse();
 		var current_time = (new Date()).getTime() / 1000;
-	    console.log("session = ", session);
-
-		return session && session.access_token && session.expires > current_time;
+		return (session && session.access_token) && (session.expires > current_time);		
 	};
 
 
 
+	window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '637163226405149', // App ID
+	     //channelUrl : 'YOUR_WEBSITE_CHANNEL_URL',
+	      status     : true, // check login status
+	      cookie     : true, // enable cookies to allow the server to access the session
+	      xfbml      : true  // parse XFBML
+	    });
+     };
+ 
+	  (function(d){
+	     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement('script'); js.id = id; js.async = true;
+	     js.src = "//connect.facebook.net/en_US/all.js";
+	     ref.parentNode.insertBefore(js, ref);
+	   }(document));
+
+
 	return{
-		globals: {},
-
-		init: function(){
-			console.log("Initializing the app...");
+	
+		init: function(callback){
+			console.log("Initializing the app..." );
 			console.log("User logged in =", isLoggedIn());
-			
-			if(isLoggedIn()){ // user is logged in so logout
-				//hello('facebook').logout('facebook',{force:true});
-				document.getElementById('loginButton').innerHTML = 'Logout';
-				return;
-			}else{ // user is logged out so login
-				document.getElementById('loginButton').innerHTML = 'Login';
-			}
-
-			hello.init({
-			    	facebook: appConf.facebookAppId
-			 	}, {
-			     display: 'page',
-			     force:true
-			 });
-
 
 			hello.on('auth.login', function(auth) {
 			    // call user information, for the given network
+			    console.log("login callback.........");
 			    hello(auth.network).api('/me').then(function(r) {
-			    		console.log("authentication successfull, data = ", r);
+			    		console.log("login callback, authentication successfull", r);
+			    		appConf.userData = r;
 			    		document.getElementById('loginButton').innerHTML = 'Logout';		    		
 				    });
 			});
 
 			hello.on('auth.logout',	function(){
-				console.log("logging out....");
+				console.log("logging out callback.........");
+				appConf.userData = false;
 				document.getElementById('loginButton').innerHTML = 'Login';
 			});
 
-			//hello('facebook').login();
+			if(isLoggedIn()){ // user is logged in so logout
+				document.getElementById('loginButton').innerHTML = 'Logout';
+				callback()
+				return;
+			}else{ // user is logged out so login
+				document.getElementById('loginButton').innerHTML = 'Login';
+				callback();
+			}
 
+			hello.init({
+					    	facebook: appConf.facebookAppId
+					 	});
+
+
+			
+
+			/*
+			hello( "facebook" ).login().then( function(){
+				console.log("logged in....");
+				callback();
+			}, function( e ){
+				console.error("Signin error: " + e.error.message );
+			});*/
+		},
+		getUserId: function(){
+			if(isLoggedIn())
+				return appConf.userData.id;
+			else
+				return 0;
+		},
+		login: function(callback){
+
+			FB.login(function(response) {
+	           if (response.authResponse) 
+	           {
+	                //getUserInfo(); // Get User Information.
+	                FB.api('/me', function(response) {
+ 						
+ 						//console.log("profile data = ", response);
+ 						callback(response);
+			        //response.name       - User Full name
+			        //response.link       - User Facebook URL
+			        //response.username   - User name
+			        //response.id         - id
+			        //response.email      - User email
+			 
+			        });	
+	 
+	            } else
+	            {
+	             console.log('Authorization failed.');
+	            }
+	         },{scope: 'email'});
+
+ 
+			// hello( "facebook" ).login().then( callback(), function(e){
+			// 		console.error( "Signed in error:" + e.error.message );
+			// 	});
+		},
+		logout: function(callback){
+			FB.logout(callback);
 		},
 
 		toggleLogin: function(){
-
+			//debugger;
 			if(isLoggedIn()){ // user is logged in so logout
-				hello('facebook').logout('facebook',{force:true});
-			}else{ // user is logged out so login
-				hello('facebook').login();
+				console.log("trying to logout...");
+				
+				hello.logout('facebook', {display:'none', force:true}, function(){
+					console.log("Signed out =" + isLoggedIn());
+				});
+			}else{ // user is logged out so login				
+				hello( "facebook" ).login().then( function(){
+					console.log("Signed in =" + isLoggedIn());
+				}, function(e){
+					console.error( "Signed in error:" + e.error.message );
+				});
+	
+				// options = {scope:'basic, friends, events, create_event, email, notifications', redirect_uri:''};
 			}
 		}
 	}
